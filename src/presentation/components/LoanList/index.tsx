1641dbf148bc322dac1e2fe6446fc61577aee6aa
Giovanni Fiori <giovannicfiori@gmail.com>
2022-05-29 08:02:18 -0400

    refactor: simplify loan list structure

--- src/presentation/components/LoanList/index.tsx
+++ src/presentation/components/LoanList/index.tsx
@@ -1,2 +1,35 @@
-export * from './LoanListContainer'
-export * from './LoanListItem'
+import React from 'react'
+import ILoan from '../../../domain/interface/ILoan'
+
+type LoanListProps = {
+  loans: Array<ILoan>
+  onLoanClicked(id: number): void
+}
+
+function LoanList({ loans, onLoanClicked }: LoanListProps) {
+  return (
+    <>
+      {loans.map((loan) => (
+        <LoanListItem key={loan.id} loan={loan} onClick={onLoanClicked} />
+      ))}
+    </>
+  )
+}
+
+type LoanItemProps = {
+  loan: ILoan
+  onClick(id: number): void
+}
+
+function LoanListItem({ loan, onClick }: LoanItemProps) {
+  return (
+    <div onClick={() => onClick(loan.id)}>
+      <p>{`${loan.description} - R$${loan.value}`}</p>
+      <span>
+        próximo vencimento: {loan.nextDueDate()?.toLocaleDateString()}
+      </span>
+    </div>
+  )
+}
+
+export default LoanList
--- src/presentation/pages/LoanListing/index.tsx
+++ src/presentation/pages/LoanListing/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import useLoanStore from '../../../data/store/loanStore'
 import Button from '../../components/Button'
-import LoanList from '../../components/LoanList/LoanListContainer'
+import LoanList from '../../components/LoanList'
 import Layout from '../../Layout'
 
 function LoanListing() {
