5337e8ec6b040febd1693aa14722c9de40d77091
Giovanni Fiori <giovannicfiori@gmail.com>
2022-05-29 07:58:55 -0400

    feat: add getLoan method to state manager

--- src/data/store/loanStore.ts
+++ src/data/store/loanStore.ts
@@ -4,14 +4,16 @@ import ILoan from '../../domain/interface/ILoan'
 type LoanState = {
   loans: Array<ILoan>
   addLoan(loan: ILoan): void
+  getLoan(id: number): ILoan | null
 }
 
-const useLoanStore = create<LoanState>((set) => ({
+const useLoanStore = create<LoanState>((set, get) => ({
   loans: [],
   addLoan: (loan) =>
     set((state) => ({
       loans: [...state.loans, loan],
     })),
+  getLoan: (id) => get().loans.find((l) => l.id === id) || null,
 }))
 
 export default useLoanStore
