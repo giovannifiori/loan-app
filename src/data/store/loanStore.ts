d2f350771f9418f860f528bd1499a04f803c7206
Giovanni Fiori <giovannicfiori@gmail.com>
2022-05-29 10:44:29 -0400

    feat: save data to local storage

--- src/data/store/loanStore.ts
+++ src/data/store/loanStore.ts
@@ -1,6 +1,8 @@
 import create from 'zustand'
 import { IInstallment } from '../../domain/interface/IInstallment'
 import ILoan from '../../domain/interface/ILoan'
+import Installment from '../../domain/model/Installment'
+import Loan from '../../domain/model/Loan'
 import { todayWithoutTime } from '../../utils/date'
 
 type LoanState = {
@@ -10,11 +12,15 @@ type LoanState = {
 }
 
 const useLoanStore = create<LoanState>((set) => ({
-  loans: [],
+  loans: getFromLocalStorage('loans') || [],
   addLoan: (loan) =>
-    set((state) => ({
-      loans: [...state.loans, loan],
-    })),
+    set((state) => {
+      const newLoans = [...state.loans, loan]
+      setToLocalStorage('loans', newLoans)
+      return {
+        loans: newLoans,
+      }
+    }),
   payLoanInstallment: (loan, installment) => {
     set((state) => {
       const loans = [...state.loans]
@@ -24,9 +30,38 @@ const useLoanStore = create<LoanState>((set) => ({
       )
       loans[loanIndex].installments[installmentIndex].paymentDate =
         todayWithoutTime()
+      setToLocalStorage('loans', loans)
       return { loans }
     })
   },
 }))
+
+function getFromLocalStorage(key: string) {
+  if (!window.localStorage.getItem(key)) return null
+  const parsed: Array<ILoan> = JSON.parse(
+    window.localStorage.getItem(key) as string
+  )
+  return parsed.map(
+    (loanJson) =>
+      new Loan(
+        loanJson.id,
+        loanJson.description,
+        loanJson.value,
+        new Date(loanJson.firstPaymentDate),
+        loanJson.installments.map(
+          (i) =>
+            new Installment(
+              i.value,
+              i.index,
+              new Date(i.dueDate),
+              i.paymentDate ? new Date(i.paymentDate) : i.paymentDate
+            )
+        )
+      )
+  )
+}
+
+const setToLocalStorage = (key: string, value: object) =>
+  window.localStorage.setItem(key, JSON.stringify(value))
 
 export default useLoanStore
